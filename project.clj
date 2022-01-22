4632c501eae769c95bcb1409eea716ed7cb5cc55
Nagarajan N <oneto018@gmail.com>
2022-01-22 13:37:22 +0530

    change desc

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject trivial-library-example "0.1.0-SNAPSHOT"
+(defproject org.clojars.oneto018/trivial-library-example "0.1.0-SNAPSHOT"
   :description "Testing publishing clojure library to clojars"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
